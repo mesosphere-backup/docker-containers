a39fa38a34d2641d9069a3cfe8597297d1b80895
Jeremy Lingmann <jeremy@mesosphere.io>
2015-04-20 17:02:43 -0700

    Support building Chronos with local deb

--- chronos/mk.sh
+++ chronos/mk.sh
@@ -6,6 +6,10 @@ set -e
 
 if [ "${#}" -ne 2 ]; then
     echo "usage: ${0} {chronos-version} {mesos-version}"
+    echo ""
+    echo "  If a deb package exists in CWD with {chronos-version} in the name,"
+    echo "  it will be used, otherwise chronos will be pulled from the package"
+    echo "  repositories."
     exit 1
 fi
 
@@ -13,11 +17,22 @@ CHRONOS_VERSION=${1}
 MESOS_VERSION=${2}
 
 FULL_VERSION="chronos-$CHRONOS_VERSION-mesos-$MESOS_VERSION"
+echo "$FULL_VERSION" > docker_tag
 
-cp chronos-template Dockerfile
+CHRONOS_PKG=$(shopt -s nullglob; echo chronos_*${CHRONOS_VERSION}*.deb)
+if test -n "$CHRONOS_PKG"
+then
+  echo "building with local chronos package: ${CHRONOS_PKG}"
+  cp chronos-local-template Dockerfile
+  sed -i -e "s/CHRONOS_PKG/${CHRONOS_PKG}/g" Dockerfile
+else
+  echo "building with chronos from package repositories"
+  cp chronos-template Dockerfile
+  sed -i -e "s/CHRONOS_VERSION/${CHRONOS_VERSION}/g" Dockerfile
+fi
 
-sed -i -e "s/CHRONOS_VERSION/${CHRONOS_VERSION}/g" Dockerfile
 sed -i -e "s/MESOS_VERSION/${MESOS_VERSION}/g" Dockerfile
+
 docker build -t "mesosphere/chronos:${FULL_VERSION}" .
 echo "to push: \"docker push mesosphere/chronos:${FULL_VERSION}\""
 rm -f Dockerfile
