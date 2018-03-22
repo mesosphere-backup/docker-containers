da02b129d9071655325e976b36a8376d2faf63ef
Yuki Ishibashi <ishibash@adobe.com>
2018-03-22 10:44:08 -0400

    Update files to build new chronos off of mesos 1.2.3

--- chronos/mk.sh
+++ chronos/mk.sh
@@ -5,20 +5,30 @@
 set -e
 
 if [ "${#}" -ne 2 ]; then
-    echo "usage: ${0} {chronos-version} {mesos-version}"
+    echo "usage: ${0} {chronos-version} {mesos-version} {repo} [{image_tag}]"
     echo ""
     echo "  If a deb package exists in CWD with {chronos-version} in the name,"
     echo "  it will be used, otherwise chronos will be pulled from the package"
     echo "  repositories."
+    echo "  For {repo} specify org/repository like 'ethos/chronos' for Adobe Platform."
+    echo "  If you want to specify non-DockerHub.com registry, use environment variable 'DOCKER_REGISTRY',"
+    echo "    Ex: 'export DOCKER_REGISTRY=docker-ethos-core-univ-release.dr-uw2.adobeitc.com' for Adobe Platform registry."
+    echo "  Specify '{image_tag}' to override default tag, which is 'chronos-{chronos_version}-mesos-{mesos-version}'."
     exit 1
 fi
 
 CHRONOS_VERSION=${1}
 MESOS_VERSION=${2}
-
+REPO=${3}
 FULL_VERSION="chronos-$CHRONOS_VERSION-mesos-$MESOS_VERSION"
-echo "$FULL_VERSION" > docker-tag
-
+if [[ "${4}" != "" ]] ; then
+  IMAGE_TAG=${4}
+else
+  IMAGE_TAG="$FULL_VERSION"
+fi
+if [[ "$DOCKER_REGISTRY" != "" ]] ; then
+  DOCKER_REGISTRY="${DOCKER_REGISTRY}/"
+echo "$IMAGE_TAG" > docker-tag
 CHRONOS_PKG=$(shopt -s nullglob; echo chronos_*${CHRONOS_VERSION}*.deb)
 if test -n "$CHRONOS_PKG"
 then
@@ -33,6 +43,6 @@ fi
 
 sed -i -e "s/MESOS_VERSION/${MESOS_VERSION}/g" Dockerfile
 
-docker build -t "behance/chronos:${FULL_VERSION}" .
-echo "to push: \"docker push behance/chronos:${FULL_VERSION}\""
+docker build -t "${DOCKER_REGISTRY}${REPO}:${IMAGE_TAG}" .
+echo "to push: \"docker push ${DOCKER_REGISTRY}${REPO}:${IMAGE_TAG}\""
 rm -f Dockerfile
